a9cec1202b6586bc2651c52a9c274dbf38a75460
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-04-01 10:10:30 -0300

    chore: remove player type

--- src/main/java/br/edu/univas/ed/domino/enums/PlayerType.java
+++
@@ -1,5 +0,0 @@
-package br.edu.univas.ed.domino.enums;
-
-public enum PlayerType {
-    HUMAN, COMPUTER
-}
--- src/main/java/br/edu/univas/ed/domino/models/Player.java
+++ src/main/java/br/edu/univas/ed/domino/models/Player.java
@@ -1,22 +1,10 @@
 package br.edu.univas.ed.domino.models;
 
-import br.edu.univas.ed.domino.enums.PlayerType;
-
 public class Player {
-    private PlayerType type;
     private List hand;
 
-    public Player(PlayerType type, List hand) {
-        this.setType(type);
+    public Player(List hand) {
         this.setHand(hand);
-    }
-
-    public PlayerType getType() {
-        return this.type;
-    }
-
-    public void setType(PlayerType type) {
-        this.type = type;
     }
 
     public List getHand() {
